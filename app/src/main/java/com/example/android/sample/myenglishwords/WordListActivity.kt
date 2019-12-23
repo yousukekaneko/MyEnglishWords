294800000ed3b230b4bec600bb1e0bd4d5ac86a2
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2019-12-23 22:26:59 +0900

    activityの追加

---
+++ app/src/main/java/com/example/android/sample/myenglishwords/WordListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.sample.myenglishwords
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WordListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_list)
+    }
+}
